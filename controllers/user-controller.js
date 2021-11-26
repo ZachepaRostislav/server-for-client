52525b243da458d1e8ecacdb8b95a47283ecba86
Rostyslav <rostyslav.zachepa@easternpeak.com>
2021-11-26 18:57:41 +0200

    brb

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,33 +1,83 @@
+const userService = require('../service/user-service')
+const { validationResult } = require('express-validator')
+const ApiError = require('../exceptions/api-error')
 class UserController {
     async registration(req, res, next) {
         try {
-        } catch (e) {}
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return next(
+                    ApiError.BadRequest('Ошибка при валидации', errors.array())
+                )
+            }
+            const { email, password } = req.body
+            const userData = await userService.registration(email, password)
+            res.cookie('refreshToken', userData.refreshToken, {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+                httpOnly: true,
+            })
+            return res.json(userData)
+        } catch (e) {
+            next(e)
+        }
     }
 
     async login(req, res, next) {
         try {
-        } catch (e) {}
+            const { email, password } = req.body;
+            const userData = await userService.login(email,password);
+            res.cookie('refreshToken', userData.refreshToken, {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+                httpOnly: true,
+            })
+            return res.json(userData)
+        } catch (e) {
+            next(e)
+        }
     }
 
     async logout(req, res, next) {
         try {
-        } catch (e) {}
+            const{refreshToken} = req.cookies;
+            const token=await userService.logout(refreshToken);
+            res.clearCookie('refreshToken');
+            return res.json(token)
+        } catch (e) {
+            next(e)
+        }
     }
 
     async activate(req, res, next) {
         try {
-        } catch (e) {}
+            const activationLink = req.params.link
+            await userService.activate(activationLink)
+            return res.redirect(process.env.CLIENT_URL)
+        } catch (e) {
+            next(e)
+        }
     }
 
     async refresh(req, res, next) {
         try {
-        } catch (e) {}
+            const {refreshToken} = req.cookies;
+            const userData = await userService.refresh(refreshToken);
+            res.cookie('refreshToken', userData.refreshToken, {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+                httpOnly: true,
+            })
+            return res.json(userData)
+        } catch (e) {
+            next(e)
+        }
     }
 
     async getUsers(req, res, next) {
         try {
-            res.json(['Privet, Poka'])
-        } catch (e) {}
+            const users = await userService.getallUsers();
+            return res.json(users)
+        } catch (e) {
+            next(e)
+        }
     }
 }
 
